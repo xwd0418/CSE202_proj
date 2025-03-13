55d35ced6dc4d2892f47a18a84bb8355e5f3dffe
Wangdong <xwd0418@gmail.com>
2025-03-12 18:18:06 -0700

    small update p3 p4

--- p3.py
+++ p3.py
@@ -3,3 +3,10 @@ EMPTY = 0
 STONE = 1
 APPLE = 2
 GRASS = 3
+
+def algo_brute_p2(matrix, start_point, initial_h):
+    
+    pass
+
+def algo_p2(matrix, start_point, initial_h):
+    pass
--- p4.py
+++ p4.py
@@ -2,3 +2,9 @@ EMPTY = 0
 STONE = 1
 APPLE = 2
 GRASS = 3
+
+# define the function as you like... i will add the brute force function later 
+
+def brute_force_p4()
+
+def algo_p4()
